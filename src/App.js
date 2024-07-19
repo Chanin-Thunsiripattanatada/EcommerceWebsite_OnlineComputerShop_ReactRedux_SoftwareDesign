b19a094f6f626476c9a96cd0e18714fe13605dd1
Chanin-Thunsiripattanatada <chanin.t@kkumail.com>
2024-07-19 02:59:46 +0000

    new CodeSpace by Aomsin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
@@ -8,11 +8,13 @@ import Content1 from './component/Content1';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Header />
-          <Content1 />
-          <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <Header/>
+        <Routes path="/">
+          <Route index element={<Content1 />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
