76800596791a9b1bff879c9b0d0249c3b4befe8a
Poompiput Malimatr <poompiput.m@kkumail.com>
2024-08-24 06:46:51 +0700

    AboutmePage

--- src/components/AboutMePage/AboutMePage.jsx
+++ src/components/AboutMePage/AboutMePage.jsx
@@ -1,22 +1,33 @@
 import React from "react";
+import '../../AboutMePage.css';
+
 
 const AboutMePage = () => {
     return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            
-                        </div>
+      <>
+        
+          <div className="slide-container">
+            <div className="slide-content">
+              <div className="card-wrapper">
+                <div className="card">
+                    <div className="image-content">
+                        <span className="overlay"> </span>
+                            <div className="card-image">
+                                <img src = {require('../../assets/image/poom.jpg')} alt="poom" className="card-image" />
+                            </div>
+                        
+                    </div>
+                    <div className="card-content">
+                        <h2 className="name">Poompiput Malimatr</h2>
+                        <p className="description">College of Computing</p>
                     </div>
                 </div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        
+      </>
+    );
 };
 
 export default AboutMePage;
